f1d88448201b9d543e13213ac5f162a842af6868
Sade Nesmith <sadework21@gmail.com>
2024-06-15 14:48:36 -0400

    settings view created

---
+++ HikeApp/Screen/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HikeApp
+//
+//  Created by sade on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+     CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
