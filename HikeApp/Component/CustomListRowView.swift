61afe15d5e5ada51567f198baf74cada5717fe8d
Sade Nesmith <sadework21@gmail.com>
2024-06-15 15:28:08 -0400

    finish settings view

--- HikeApp/Component/CustomListRowView.swift
+++ HikeApp/Component/CustomListRowView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct CustomListRowView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+  // MARK: - PROPERTIES
+  @State var rowLabel: String
+  @State var rowIcon: String
+  @State var rowContent: String? = nil
+  @State var rowTintColor: Color
+  @State var rowLinkLabel: String? = nil
+  @State var rowLinkDestination: String? = nil
+
+  var body: some View {
+    LabeledContent {
+      //Content
+      if rowContent != nil {
+        Text(rowContent!)
+          .foregroundColor(.primary)
+          .fontWeight(.heavy)
+      } else if (rowLinkLabel != nil && rowLinkDestination != nil) {
+        Link(rowLinkLabel!, destination: URL(string: rowLinkDestination!)!)
+          .foregroundColor(.pink)
+          .fontWeight(.heavy)
+      } else {
+        /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+      }
+    } label: {
+      HStack {
+        ZStack {
+          RoundedRectangle(cornerRadius: 8)
+            .frame(width: 30, height: 30)
+            .foregroundColor(rowTintColor)
+          Image(systemName: rowIcon)
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
+        }
+
+        Text(rowLabel)
+      }
     }
+  }
 }
 
 #Preview {
-    CustomListRowView()
+  List {
+    CustomListRowView(rowLabel: "Website",
+                      rowIcon: "globe",
+                      rowContent: nil,
+                      rowTintColor: .pink,
+                      rowLinkLabel: "Credo Academy",
+                      rowLinkDestination: "http://credo.academy"
+    )
+  }
 }
