5ca2e773f04acedd4169636bfd1301aefc203f03
Sade Nesmith <sadework21@gmail.com>
2024-06-15 14:00:16 -0400

    motion and circle animation completed

---
+++ HikeApp/Component/CustomCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleView.swift
+//  HikeApp
+//
+//  Created by sade on 6/15/24.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ HikeApp/Component/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  HikeApp
+//
+//  Created by sade on 6/15/24.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
