58798b39db1d0bf4fd20555a198c7b0b7b9c7840
Sade Nesmith <sadework21@gmail.com>
2024-06-15 15:17:22 -0400

    list row implemented

---
+++ HikeApp/Component/CustomListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRowView.swift
+//  HikeApp
+//
+//  Created by sade on 6/15/24.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRowView()
+}
